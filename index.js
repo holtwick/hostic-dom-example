fad6ec123aa7f9c96c00905e6a6efcdc9b9fe3e7
Dirk Holtwick <dirk.holtwick@gmail.com>
2020-09-07 06:38:20 +0200

    Init

---
+++ index.js
@@ -0,0 +1,43 @@
+import { h, xml, vdom, tidyDOM } from 'hostic-dom'
+
+{
+    let dom = h('ol', {
+        class: 'projects'
+    }, [
+        h('li', null, 'hostic ',
+            h('img', { src: 'logo.png' })),
+        h('li', null, 'hostic-dom'),
+    ])
+
+    console.log(dom.render())
+    console.log(dom.render(xml))
+}
+
+{
+    let dom = <ol className="projects">
+        <li>hostic</li>
+        <li>hostic-dom</li>
+    </ol>
+
+    let projects = dom.querySelectorAll('li').map(e => e.textContent).join(', ')
+    console.log(projects)
+
+    dom.handle('li', e => {
+        if (!e.textContent.endsWith('-dom')) {
+            e.remove()
+        } else {
+            e.innerHTML = '<b>hostic-dom</b> - great DOM helper for static content' 
+        }
+    })
+
+    console.log(dom.render())
+    // Output: <ol class="projects"><li><b>hostic-dom</b> - great DOM helper for static content</li></ol>
+}
+
+{
+    let dom = vdom('<div>Hello World</div>')
+    console.log(dom.render())
+    tidyDOM(dom)
+    console.log(dom.render())
+    // Output: <ol class="projects"><li><b>hostic-dom</b> - great DOM helper for static content</li></ol>
+}
